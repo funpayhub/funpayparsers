8bd9b3ccce275c06850abfdb8d4230e1eb81455e
Qvvonk <qvvonk@gmail.com>
2025-05-27 19:34:01 +0300

    Doc-string for MoneyValue

--- funpayparsers/types/common.py
+++ funpayparsers/types/common.py
@@ -7,11 +7,28 @@ from .base import FunPayObject, FunPayObjectStructure
 
 
 class MoneyValueStructure(FunPayObjectStructure):
+    """
+    A TypedDict that defines the dictionary representation of a `MoneyValue` object.
+    """
     value: int | float
     currency: Literal['USD', 'EUR', 'RUB', 'UAH', 'UNK']
 
 
 @dataclass
 class MoneyValue(FunPayObject[MoneyValueStructure]):
+    """
+    Represents a monetary value with an associated currency.
+
+    This class is used to store money-related information, such as:
+    - the price of a lot,
+    - the total of an order,
+    - the user balance,
+    - etc.
+    """
+
     value: int | float
+    """The numeric amount of the monetary value."""
+
     currency: Literal['USD', 'EUR', 'RUB', 'UAH', 'UNK']
+    """The currency of the value. 'UNK' is used for unknown currency."""
+
