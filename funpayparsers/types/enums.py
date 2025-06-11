8f6cb086ecd98bc7d6bb506fb79d3c2973354781
Qvvonk <qvvonk@gmail.com>
2025-06-11 18:00:14 +0300

    Small impovments in enums (not done)

--- funpayparsers/types/enums.py
+++ funpayparsers/types/enums.py
@@ -1,15 +1,45 @@
 __all__ = ('OrderStatus', 'Currency', 'TransactionStatus', 'PaymentMethod')
 
 
-from enum import Enum
+from enum import StrEnum, Enum, verify, UNIQUE
 
 
-class OrderStatus(Enum):
-    PAID = 0
-    COMPLETE = 1
-    REFUNDED = 2
+@verify(UNIQUE)
+class OrderStatus(StrEnum):
+    PAID = 'text-primary'
+    COMPLETE = 'text-success'
+    REFUNDED = 'text-warning'
+    UNKNOWN = 'unknown'
 
 
+
+    @staticmethod
+    def get_by_css_class(css_class: str) -> 'OrderStatus':
+        """
+        Determines the order status based on a given CSS class string.
+
+        >>> OrderStatus.get_by_css_class('text-primary some_another_css_class')
+        <OrderStatus.PAID: 'text-primary'>
+
+        >>> OrderStatus.get_by_css_class('some_another_css_class text-warning')
+        <OrderStatus.REFUNDED: 'text-warning'>
+
+        >>> OrderStatus.get_by_css_class('some_another_css_class text-success')
+        <OrderStatus.COMPLETE: 'text-success'>
+
+        >>> OrderStatus.get_by_css_class('some_another_css_class')
+        <OrderStatus.UNKNOWN: 'unknown'>
+        """
+        for i in OrderStatus:
+            if i is OrderStatus.UNKNOWN:
+                continue
+
+            if i.value in css_class:
+                return i
+        return OrderStatus.UNKNOWN
+
+
+@verify(UNIQUE)
 class Currency(Enum):
     UNK = 0
     RUB = 1
@@ -18,9 +48,11 @@ class Currency(Enum):
     UAH = 4
 
 
+@verify(UNIQUE)
 class TransactionStatus(Enum):
     ...
 
 
+@verify(UNIQUE)
 class PaymentMethod(Enum):
     ...
