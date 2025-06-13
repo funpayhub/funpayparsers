5b1c8264dc8543e2ecf19ef882726bc700573a1c
qvvonk <qvvonk@gmail.com>
2025-06-13 09:46:54 +0300

    Type hints for base parser.

--- funpayparsers/parsers/base.py
+++ funpayparsers/parsers/base.py
@@ -1,7 +1,12 @@
 from abc import ABC, abstractmethod
+from typing import TypeVar, Generic
+from ..types.base import FunPayObject
 
 
-class FunPayObjectParser(ABC):
+T = TypeVar('T', bound=FunPayObject)
+
+
+class FunPayObjectParser(ABC, Generic[T]):
     def __init__(self, raw_source: str):
         """
         :param raw_source: raw source of an object (HTML / JSON string)
@@ -9,8 +14,8 @@ class FunPayObjectParser(ABC):
         self.raw_source = raw_source
 
     @abstractmethod
-    def parse(self):
+    def parse(self) -> T:
         raise NotImplementedError()
 
-    async def async_parse(self):
+    async def async_parse(self) -> T:
         return self.parse()
