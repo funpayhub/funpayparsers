304ecdc58ee223649bd9e1f172f90b7d6ceb295d
PythonPapochka <187691553+PythonPapochka@users.noreply.github.com>
2025-06-20 00:24:30 +0300

    added tests for resolve_messages_senders

--- tests/resolve_messages_sender_test.py
+++ tests/resolve_messages_sender_test.py
@@ -2,22 +2,23 @@ import pytest
 from funpayparsers.parsers.utils import resolve_messages_senders
 from funpayparsers.types import Message, UserBadge
 
-@pytest.fixture
-def data():
-    support_badge = UserBadge(
-        raw_source='',
-        text='поддержка',
-        css_class='label-success'
-    )
+# todo add it to fixture
+data_badge = UserBadge(
+    raw_source='',
+    text='поддержка',
+    css_class='label-success'
+)
 
-    original_messages = [
+@pytest.fixture
+def original_messages():
+    messages = [
         Message(
             raw_source='',
             id=1,
             is_heading=True,
             sender_id=1,
             sender_username='SomeUser1',
-            badge=support_badge,
+            badge=data_badge,
             send_date_text='01.01.2077',
             text='MessageText1',
             image_url=None
@@ -35,34 +36,33 @@ def data():
         )
     ]
 
-    processed_messages = [
-        Message(
-            raw_source='',
-            id=1,
-            is_heading=True,
-            sender_id=1,
-            sender_username='SomeUser1',
-            badge=support_badge,
-            send_date_text='01.01.2077',
-            text='MessageText1',
-            image_url=None
-        ),
-        Message(
-            raw_source='',
-            id=2,
-            is_heading=False,
-            sender_id=1,
-            sender_username='SomeUser1',
-            badge=support_badge,
-            send_date_text='01.01.2077',
-            text='MessageText2',
-            image_url=None
-        )
-    ]
+    return messages
 
-    return original_messages, processed_messages
+processed_messages = [
+    Message(
+        raw_source='',
+        id=1,
+        is_heading=True,
+        sender_id=1,
+        sender_username='SomeUser1',
+        badge=data_badge,
+        send_date_text='01.01.2077',
+        text='MessageText1',
+        image_url=None
+    ),
+    Message(
+        raw_source='',
+        id=2,
+        is_heading=False,
+        sender_id=1,
+        sender_username='SomeUser1',
+        badge=data_badge,
+        send_date_text='01.01.2077',
+        text='MessageText2',
+        image_url=None
+    )
+]
 
-def test_message_sender_resolver(data):
-    original_messages, processed_messages = data
+def test_message_sender_resolver(original_messages):
     resolve_messages_senders(original_messages)
     assert original_messages == processed_messages
