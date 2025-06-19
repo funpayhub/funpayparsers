bd732628557e91ff667e1b1cc796402846bd8465
Qvvonk <qvvonk@gmail.com>
2025-06-19 22:09:49 +0300

    Docs for `types.enums.PaymentMethod` (#28)
    Co-authored-by: PythonPapochka <187691553+PythonPapochka@users.noreply.github.com>

--- funpayparsers/types/enums.py
+++ funpayparsers/types/enums.py
@@ -188,40 +188,95 @@ class PaymentMethod(Enum):
         - CSS: https://funpay.com/687/css/main.css
     """
 
-    QIWI = ('payment-method-1', 'payment-method-qiwi')  # 345, 94
-    YANDEX = ('payment-method-2', 'payment-method-yandex', 'payment-method-fps')  #345, 34
-    FPS = ('payment-method-21',)  # 345, 364
+    QIWI = ('payment-method-1', 'payment-method-qiwi')
+    """Qiwi wallett payment method. Sprite coords (see `PaymentMethod` doc-string): x=345, y=94."""
 
-    WEBMONEY_WME = ('payment-method-3', 'payment-method-wme')  # 345, 64
-    WEBMONEY_WMP = ('payment-method-4', 'payment-method-wmp')  # 345, 64
-    WEBMONEY_WMR = ('payment-method-5', 'payment-method-wmr')  # 345, 64
-    WEBMONEY_WMZ = ('payment-method-6', 'payment-method-wmz')  # 345, 64
-    WEBMONEY_UNKNOWN = ('payment-method-10',)  # 345, 64
+    YANDEX = ('payment-method-2', 'payment-method-yandex', 'payment-method-fps')
+    """Yandex payment method. Sprite coords (see `PaymentMethod` doc-string): x=345, y=34."""
 
-    CARD_RUB = ('payment-method-7', 'payment-method-card_rub')  # 345, 4
-    CARD_USD = ('payment-method-card_usd',)  # 345, 4
-    CARD_EUR = ('payment-method-card_eur',)  # 345, 4
-    CARD_UAH = ('payment-method-card_uah',)  # 345, 4
-    CARD_UNKNOWN = ('payment-method-11', 'payment-method-15', 'payment-method-16',
-                    'payment-method-25', 'payment-method-26', 'payment-method-27',
-                    'payment-method-32', 'payment-method-33', 'payment-method-34', 'payment-method-35',
-                    'payment-method-37', 'payment-method-38', 'payment-method-39', 'payment-method-40')  # 345, 4
+    FPS = ('payment-method-21',)
+    """FPS (what) payment method. Sprite coords (see `PaymentMethod` doc-string): x=345, y=364."""
 
-    MOBILE = ('payment-method-8',)  # 345, 124
-    APPLE = ('payment-method-9', 'payment-method-19', 'payment-method-20')  # 345, 154
-    MASTERCARD = ('payment-method-12', 'payment-method-22', 'payment-method-23')  # 345, 274
-    VISA = ('payment-method-13', 'payment-method-28', 'payment-method-29')  # 345, 304
-    GOOGLE = ('payment-method-14', 'payment-method-17', 'payment-method-18')  # 345, 244
-    FUNPAY = ('payment-method-24',)  # 345, 214
-    LITECOIN = ('payment-method-30',)  # 375, 4
-    BINANCE = ('payment-method-31',)  # 375, 34
-    BINANCE_USDT = ('payment-method-binance_usdt',)  # 375, 34
-    BINANCE_USDC = ('payment-method-binance_usdc',)  # 375, 34
-    PAYPAL = ('payment-method-36', 'payment-method-paypal')  # 345, 184
-    USDT_TRC = ('payment-method-usdt_trc',)  # 375, 64
+    WEBMONEY_WME = ('payment-method-3', 'payment-method-wme')
+    """WebMoney WME payment method. Sprite coords (see `PaymentMethod` doc-string): x=345, y=64."""
+
+    WEBMONEY_WMP = ('payment-method-4', 'payment-method-wmp')
+    """WebMoney WMP payment method. Sprite coords (see `PaymentMethod` doc-string): x=345, y=64."""
+
+    WEBMONEY_WMR = ('payment-method-5', 'payment-method-wmr')
+    """WebMoney WMR payment method. Sprite coords (see `PaymentMethod` doc-string): x=345, y=64."""
+
+    WEBMONEY_WMZ = ('payment-method-6', 'payment-method-wmz')
+    """WebMoney WMZ payment method. Sprite coords (see `PaymentMethod` doc-string): x=345, y=64."""
+
+    WEBMONEY_UNKNOWN = ('payment-method-10',)
+    """WebMoney unknown type. Sprite coords (see `PaymentMethod` doc-string): x=345, y=64."""
+
+    CARD_RUB = ('payment-method-7', 'payment-method-card_rub')
+    """Card RUB payment method. Sprite coords (see `PaymentMethod` doc-string): x=345, y=4."""
+
+    CARD_USD = ('payment-method-card_usd',)
+    """Card USD payment method. Sprite coords (see `PaymentMethod` doc-string): x=345, y=4."""
+
+    CARD_EUR = ('payment-method-card_eur',)
+    """Card EUR payment method. Sprite coords (see `PaymentMethod` doc-string): x=345, y=4."""
+
+    CARD_UAH = ('payment-method-card_uah',)
+    """Card UAH payment method. Sprite coords (see `PaymentMethod` doc-string): x=345, y=4."""
+
+    CARD_UNKNOWN = (
+        'payment-method-11', 'payment-method-15', 'payment-method-16',
+        'payment-method-25', 'payment-method-26', 'payment-method-27',
+        'payment-method-32', 'payment-method-33', 'payment-method-34', 'payment-method-35',
+        'payment-method-37', 'payment-method-38', 'payment-method-39', 'payment-method-40'
+    )
+    """Unknown card, maybe it will added soon. Sprite coords (see `PaymentMethod` doc-string): x=345, y=4."""
+
+    MOBILE = ('payment-method-8',)
+    """Mobile payment method. Sprite coords (see `PaymentMethod` doc-string): x=345, y=124."""
+
+    APPLE = ('payment-method-9', 'payment-method-19', 'payment-method-20')
+    """Apple Pay payment method. Sprite coords (see `PaymentMethod` doc-string): x=345, y=154."""
+
+    MASTERCARD = ('payment-method-12', 'payment-method-22', 'payment-method-23')
+    """MasterCard payment method. Sprite coords (see `PaymentMethod` doc-string): x=345, y=274."""
+
+    VISA = ('payment-method-13', 'payment-method-28', 'payment-method-29')
+    """Visa payment method. Sprite coords (see `PaymentMethod` doc-string): x=345, y=304."""
+
+    GOOGLE = ('payment-method-14', 'payment-method-17', 'payment-method-18')
+    """Google Pay payment method. Sprite coords (see `PaymentMethod` doc-string): x=345, y=244."""
+
+    FUNPAY = ('payment-method-24',)
+    """
+    FunPay balance payment method.
+    You can pay from your balance if the funds remain on your balance for
+    48 hours from the moment of receipt, or if you have an instant withdraw.
+    Sprite coords (see `PaymentMethod` doc-string): x=345, y=214.
+    """
+
+    LITECOIN = ('payment-method-30',)
+    """Litecoin (LTC) payment method. Sprite coords (see `PaymentMethod` doc-string): x=375, y=4."""
+
+    BINANCE = ('payment-method-31',)
+    """Binance generic payment method. Sprite coords (see `PaymentMethod` doc-string): x=375, y=34."""
+
+    BINANCE_USDT = ('payment-method-binance_usdt',)
+    """Binance USDT payment method. Sprite coords (see `PaymentMethod` doc-string): x=375, y=34."""
+
+    BINANCE_USDC = ('payment-method-binance_usdc',)
+    """Binance USDC payment method. Sprite coords (see `PaymentMethod` doc-string): x=375, y=34."""
+
+    PAYPAL = ('payment-method-36', 'payment-method-paypal')
+    """PayPal payment method. Sprite coords (see `PaymentMethod` doc-string): x=345, y=184."""
+
+    USDT_TRC = ('payment-method-usdt_trc',)
+    """USDT TRC-20 payment method. Sprite coords (see `PaymentMethod` doc-string): x=375, y=64."""
+
     UNKNOWN = ('',)
+    """Unknown payment method."""
 
-    # MIR = 26, ('UNKNOWN', ), (345, Y)
+    # MIR = 26, ('UNKNOWN', ), (345, Y) =(
 
     @staticmethod
     @cache
