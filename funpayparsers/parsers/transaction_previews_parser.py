cfe58e41ee20d91974a24dae3f9255428f8b69a4
qvvonk <qvvonk@gmail.com>
2025-06-20 11:08:04 +0300

    TransactionPreviewsParser

--- funpayparsers/parsers/transaction_previews_parser.py
+++ funpayparsers/parsers/transaction_previews_parser.py
@@ -2,7 +2,10 @@ __all__ = ('TransactionPreviewsParser', 'TransactionPreviewsParserOptions')
 
 from dataclasses import dataclass
 from funpayparsers.parsers.base import FunPayObjectParser, FunPayObjectParserOptions
-from funpayparsers.types import TransactionPreview, TransactionStatus
+from funpayparsers.types.finances import TransactionPreview
+from funpayparsers.types.enums import TransactionStatus, PaymentMethod
+
+from lxml import html
 
 
 @dataclass(frozen=True)
@@ -18,4 +21,29 @@ class TransactionPreviewsParser(FunPayObjectParser[
     __options_cls__ = TransactionPreviewsParserOptions
 
     def _parse(self):
-        ...
+        result = []
+        for i in self._tree.xpath('.//div[contains(@class, "tc-item")]'):
+            id_ = int(i.get('data-transaction'))
+            transaction_class_name = i.get('class').split()[1]  # "tc-item <transaction_class>"
+
+            desc: str = i.xpath('string(.//span[@class="tc-title"][1])')
+            date: str = i.xpath('string(.//span[@class="tc-date-time"][1])')
+
+            # Depending on the type of transaction, the recipient's line may or may not be present.
+            # The recipient's line exists only in TransactionType.WITHDRAWAL transactions.
+            recipient = i.xpath('string(.//span[@class="tc-payment-number"][1])') or None
+
+            payment_method = i.xpath('string(.//span[contains(@class, "payment-logo")][1]/@class)')
+            payment_method = None if not payment_method else PaymentMethod.get_by_css_class(payment_method)
+
+            result.append(TransactionPreview(
+                raw_source=html.tostring(i, encoding='unicode'),
+                id=id_,
+                date_text=date,
+                desc=desc,
+                status=TransactionStatus.get_by_css_class(transaction_class_name),
+                amount=...,  # todo
+                payment_method=payment_method,
+                withdrawal_number=recipient
+            ))
+        return result
