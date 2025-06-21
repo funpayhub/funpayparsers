2d2c8e70392de72a2459e41582c701dbfe7c97e5
qvvonk <qvvonk@gmail.com>
2025-06-21 06:42:33 +0300

    Parser scratch

--- funpayparsers/parsers/money_value_parser.py
+++ funpayparsers/parsers/money_value_parser.py
@@ -4,11 +4,20 @@ from lxml import html
 from dataclasses import dataclass
 from funpayparsers.parsers.base import FunPayObjectParserOptions, FunPayObjectParser, T
 from funpayparsers.types.common import MoneyValue
+from enum import Enum
+
+
+class MoneyValueParsingType(Enum):
+    FROM_ORDER_PREVIEW = 0
+    FROM_TRANSACTION_PREVIEW = 1
+    FROM_LOT_PREVIEW = 2
+    FROM_LOT = 3
+    FROM_STRING = 4
 
 
 @dataclass(frozen=True)
 class MoneyValueParserOptions(FunPayObjectParserOptions):
-    ...
+    parsing_type: MoneyValueParsingType = MoneyValueParsingType.FROM_STRING
 
 
 class MoneyValueParser(FunPayObjectParser[MoneyValue, MoneyValueParserOptions]):
@@ -16,4 +25,25 @@ class MoneyValueParser(FunPayObjectParser[MoneyValue, MoneyValueParserOptions]):
     __options_cls__ = MoneyValueParserOptions
 
     def _parse(self):
+        types = {
+            MoneyValueParsingType.FROM_ORDER_PREVIEW: self._parse_order_preview_type,
+            MoneyValueParsingType.FROM_TRANSACTION_PREVIEW: self._parse_transaction_preview_type,
+            MoneyValueParsingType.FROM_LOT_PREVIEW: self._parse_lot_preview_type,
+            MoneyValueParsingType.FROM_LOT: self._parse_lot_type,
+            MoneyValueParsingType.FROM_STRING: self._parse_string_type,
+        }
+
+    def _parse_order_preview_type(self):
+        ...
+
+    def _parse_transaction_preview_type(self):
+        ...
+
+    def _parse_lot_preview_type(self):
+        ...
+
+    def _parse_lot_type(self):
+        ...
+
+    def _parse_string_type(self):
         ...
