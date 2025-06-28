e9aec6e0584177bf1e12d7ae8c29161c37424a06
qvvonk <qvvonk@gmail.com>
2025-06-28 12:18:21 +0300

    Small changes

--- funpayparsers/parsers/base.py
+++ funpayparsers/parsers/base.py
@@ -1,7 +1,7 @@
 __all__ = ('FunPayHTMLObjectParser', 'FunPayObjectParserOptions')
 
 from abc import ABC, abstractmethod
-from typing import get_args, get_origin, ParamSpec
+from typing import get_args, get_origin
 from dataclasses import dataclass, replace, fields, asdict, field
 from typing import Generic, Type, TypeVar, Any
 from collections.abc import Sequence, Mapping
@@ -32,11 +32,11 @@ class FunPayObjectParserOptions:
 
 
 
-T = TypeVar('T', bound=Any)
-P = TypeVar('P', bound=FunPayObjectParserOptions)
+R = TypeVar('R', bound=Any)
+O = TypeVar('O', bound=FunPayObjectParserOptions)
 
 
-class FunPayObjectParser(ABC, Generic[T, P]):
+class FunPayObjectParser(ABC, Generic[R, O]):
     """
     Base class for all parsers.
 
@@ -50,17 +50,17 @@ class FunPayObjectParser(ABC, Generic[T, P]):
     """
     __options_cls__: Type[FunPayObjectParserOptions] | None = None
 
-    def __init__(self, raw_source: Any, options: P | None = None, **overrides):
+    def __init__(self, raw_source: Any, options: O | None = None, **overrides):
         """
         :param raw_source: raw source of an object (HTML / JSON string)
         """
         self._raw_source = raw_source
-        self._options: P = self._build_options(options, **overrides)
+        self._options: O = self._build_options(options, **overrides)
 
     @abstractmethod
-    def _parse(self) -> T: ...
+    def _parse(self) -> R: ...
 
-    def parse(self) -> T:
+    def parse(self) -> R:
         try:
             result = self._parse()
 
@@ -73,21 +73,19 @@ class FunPayObjectParser(ABC, Generic[T, P]):
             raise e  # todo: make custom exceptions e.g. ParsingError
 
     def empty_raw_source(self,
-                         obj: FunPayObject |
-                              Sequence[FunPayObject] |
-                              Mapping[Any, FunPayObject]) -> None:
-        if hasattr(type(obj), '__dataclass_fields__'):  # if dataclass
+                         obj: FunPayObject | Sequence[Any] | Mapping[Any, Any]) -> None:
+        if hasattr(type(obj), '__dataclass_fields__'):
             if hasattr(obj, 'raw_source'):
                 setattr(obj, 'raw_source', '')
 
             for f in fields(obj):
                 self.empty_raw_source(getattr(obj, f.name))
 
-        elif isinstance(obj, (list, tuple)):
+        elif isinstance(obj, Sequence):
             for item in obj:
                 self.empty_raw_source(item)
 
-        elif isinstance(obj, dict):
+        elif isinstance(obj, Mapping):
             for item in obj.values():
                 self.empty_raw_source(item)
 
@@ -96,18 +94,18 @@ class FunPayObjectParser(ABC, Generic[T, P]):
         return self._raw_source
 
     @property
-    def options(self) -> P:
+    def options(self) -> O:
         return self._options
 
     @classmethod
-    def _build_options(cls, options: P | None, **overrides) -> P:
+    def _build_options(cls, options: O | None, **overrides) -> O:
         base = options or cls.get_options_cls()()
         overrides = {k: v for k, v in overrides.items() if
                      k in getattr(base, '__dataclass_fields__', {})}
         return replace(base, **overrides)
 
     @classmethod
-    def get_options_cls(cls) -> Type[P]:
+    def get_options_cls(cls) -> Type[O]:
         if cls.__options_cls__ is not None:
             return cls.__options_cls__
 
@@ -119,7 +117,7 @@ class FunPayObjectParser(ABC, Generic[T, P]):
                               f'Try explicitly specifying `__options_cls__` in `{cls.__name__}`.') from e
 
     @classmethod
-    def _get_options_cls_inner(cls) -> Type[P]:
+    def _get_options_cls_inner(cls) -> Type[O]:
         parents = getattr(cls, '__orig_bases__', ())
         for parent in parents:
             origin, args = get_origin(parent), get_args(parent)
@@ -135,8 +133,8 @@ class FunPayObjectParser(ABC, Generic[T, P]):
         raise LookupError('No suitable options class found.')
 
 
-class FunPayHTMLObjectParser(FunPayObjectParser[T, P], ABC):
-    def __init__(self, raw_source: str, options: P | None = None, **overrides):
+class FunPayHTMLObjectParser(FunPayObjectParser[R, O], ABC):
+    def __init__(self, raw_source: str, options: O | None = None, **overrides):
         """
         :param raw_source: raw source of an object (HTML / JSON string)
         """
@@ -159,8 +157,8 @@ class FunPayHTMLObjectParser(FunPayObjectParser[T, P], ABC):
         return self._raw_source
 
 
-class FunPayJSONObjectParser(FunPayHTMLObjectParser[T, P], ABC):
-    def __init__(self, raw_source: str | dict | list, options: P | None = None, **overrides):
+class FunPayJSONObjectParser(FunPayHTMLObjectParser[R, O], ABC):
+    def __init__(self, raw_source: str | dict | list, options: O | None = None, **overrides):
         super().__init__(raw_source=raw_source,
                          options=options,
                          **overrides)
