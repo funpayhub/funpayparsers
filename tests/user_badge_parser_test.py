f2fa05fbc260483c450464a84468f074eeaef1ae
qvvonk <qvvonk@gmail.com>
2025-07-01 14:22:08 +0300

    User badge parser tests improvements

--- tests/user_badge_parser_test.py
+++ tests/user_badge_parser_test.py
@@ -1,27 +1,29 @@
+import pytest
 from funpayparsers.parsers import UserBadgeParser, UserBadgeParserOptions
 from funpayparsers.types import UserBadge
 
 
 OPTIONS = UserBadgeParserOptions(empty_raw_source=True)
 
-
-banned_badge_html = """<span class="label label-danger">заблокирован</span>"""
-auto_issue_badge_html = """<span class="chat-msg-author-label label label-default">автоответ</span>"""
-support_badge_html = """<span class="chat-msg-author-label label label-success">поддержка</span>"""
-
-
-banned_badge_obj = UserBadge('', 'заблокирован', 'label label-danger')
-auto_issue_badge_obj = UserBadge('', 'автоответ', 'chat-msg-author-label label label-default')
-support_badge_obj = UserBadge('', 'поддержка', 'chat-msg-author-label label label-success')
-
-
-def test_banned_badge_parsing():
-    assert UserBadgeParser(banned_badge_html, options=OPTIONS).parse() == banned_badge_obj
+@pytest.mark.parametrize(
+    'html,obj',
+    [
+        ('<span class="label css_class">BadgeText</span>',
+         UserBadge(raw_source='', css_class='label css_class', text='BadgeText')),
+    ]
+)
+def test_badge_parser(html, obj):
+    assert UserBadgeParser(html, options=OPTIONS).parse() == obj
 
 
-def test_auto_issue_badge_parsing():
-    assert UserBadgeParser(auto_issue_badge_html, options=OPTIONS).parse() == auto_issue_badge_obj
+@pytest.mark.parametrize(
+    'html',
+    [
+        ('<span class="label css_class">BadgeText</span>',)
+    ]
+)
+def test_badge_parser_raise_error_if_no_label_css_class(html):
+    with pytest.raises(Exception) as e:
+        UserBadgeParser(html, options=OPTIONS).parse()
 
-
-def test_support_badge_parsing():
-    assert UserBadgeParser(support_badge_html, options=OPTIONS).parse() == support_badge_obj
+    # todo: make custom parsing exception and assertation for this exception
