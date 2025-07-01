a683911175e6e9ea45744d89f91a7f40773742b0
Qvvonk <qvvonk@gmail.com>
2025-07-01 15:15:19 +0300

    Tests (#47)
    1. Added tests for enums.
    2. Parsers tests moved to separate folder.
    3. Improved badges parser test.
    4. Added raises test for badges parser.

---
+++ tests/enums_test/update_type_test.py
@@ -0,0 +1,17 @@
+import pytest
+from funpayparsers.types.enums import UpdateType
+
+
+@pytest.mark.parametrize(
+    'update_type_str,expected_value',
+    [
+        ('orders_counters', UpdateType.ORDERS_COUNTERS),
+        ('chat_counter', UpdateType.CHAT_COUNTER),
+        ('chat_bookmarks', UpdateType.CHAT_BOOKMARKS),
+        ('chat_node', UpdateType.CHAT_NODE),
+        ('c-p-u', UpdateType.CPU),
+        ('some-unknown-update-type', None),
+    ]
+)
+def test_update_type_determination(update_type_str, expected_value):
+    assert UpdateType.get_by_type_str(update_type_str) is expected_value
