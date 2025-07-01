63a267d578976ebad0470d20828f0b851daa43d8
qvvonk <qvvonk@gmail.com>
2025-07-01 22:49:44 +0300

    Small docs changes

--- funpayparsers/types/categories.py
+++ funpayparsers/types/categories.py
@@ -29,8 +29,14 @@ class Subcategory(FunPayObject):
     """
     Subcategory ID.
 
-    :warning: Subcategory ID is not unique. 
+    :warning: Subcategory ID is not always unique. 
     IDs are unique per subcategory type but may repeat across types.
+    That means, that some common category (`CategoryType.COMMON`) 
+    can have same ID as some currency category (`CategoryType.CURRENCY`).
+    
+    Example:
+        Common category "Lineage 2 Items (RU)" (category ID: 1): https://funpay.com/lots/1/
+        Currency category "Lineage 2 Adena (RU)" (category ID: 1): https://funpay.com/chips/1/
     """
 
     name: str
