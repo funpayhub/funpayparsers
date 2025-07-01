14a89ec41e7cfa177903dcef79da27abeab810bd
qvvonk <qvvonk@gmail.com>
2025-07-01 15:08:14 +0300

    Badge Parser tests to class

--- tests/user_badge_parser_test.py
+++ tests/user_badge_parser_test.py
@@ -3,27 +3,29 @@ from funpayparsers.parsers import UserBadgeParser, UserBadgeParserOptions
 from funpayparsers.types import UserBadge
 
 
-OPTIONS = UserBadgeParserOptions(empty_raw_source=True)
 
-@pytest.mark.parametrize(
-    'html,obj',
-    [
-        ('<span class="label css_class">BadgeText</span>',
-         UserBadge(raw_source='', css_class='label css_class', text='BadgeText')),
-    ]
-)
-def test_badge_parser(html, obj):
-    assert UserBadgeParser(html, options=OPTIONS).parse() == obj
+class TestUserBadgeParser:
+    OPTIONS = UserBadgeParserOptions(empty_raw_source=True)
+
+    @pytest.mark.parametrize(
+        'html,obj',
+        [
+            ('<span class="label css_class">BadgeText</span>',
+             UserBadge(raw_source='', css_class='label css_class', text='BadgeText')),
+        ]
+    )
+    def test_badge_parser(self, html, obj):
+        assert UserBadgeParser(html, options=self.OPTIONS).parse() == obj
 
 
-@pytest.mark.parametrize(
-    'html',
-    [
-        ('<span class="label css_class">BadgeText</span>',)
-    ]
-)
-def test_badge_parser_raise_error_if_no_label_css_class(html):
-    with pytest.raises(Exception) as e:
-        UserBadgeParser(html, options=OPTIONS).parse()
+    @pytest.mark.parametrize(
+        'html',
+        [
+            ('<span class="label css_class">BadgeText</span>',)
+        ]
+    )
+    def test_badge_parser_raise_error_if_no_label_css_class(self, html):
+        with pytest.raises(Exception) as e:
+            UserBadgeParser(html, options=self.OPTIONS).parse()
 
-    # todo: make custom parsing exception and assertation for this exception
+        # todo: make custom parsing exception and assertation for this exception
