7ff7e02257a8f3d8c8b0c5af01c700ffc2ebe014
qvvonk <qvvonk@gmail.com>
2025-07-04 23:59:48 +0300

    MainPageParser

---
+++ funpayparsers/parsers/pages_parsers/main_page_parser.py
@@ -0,0 +1,47 @@
+__all__ = ('MainPageParser', 'MainPageParserOptions')
+
+from dataclasses import dataclass
+from funpayparsers.parsers.base import FunPayHTMLObjectParser, FunPayObjectParserOptions
+from funpayparsers.parsers.categories_parser import CategoriesParser, CategoriesParserOptions
+from funpayparsers.parsers.chat_parser import ChatParser, ChatParserOptions
+from funpayparsers.parsers.page_header_parser import PageHeaderParser, PageHeaderParserOptions
+from funpayparsers.types.pages.main_page import MainPage
+
+
+@dataclass(frozen=True)
+class MainPageParserOptions(FunPayObjectParserOptions):
+    header_parser_options: PageHeaderParserOptions = PageHeaderParserOptions()
+    categories_parser_options: CategoriesParserOptions = CategoriesParserOptions()
+    chat_parser_options: ChatParserOptions = ChatParserOptions()
+
+
+class MainPageParser(FunPayHTMLObjectParser[MainPage, MainPageParserOptions]):
+    """
+    Class for parsing FunPay main page.
+    Possible locations:
+        - https://funpay.com/
+    """
+
+    def _parse(self):
+        header_div = self.tree.css('header')[0]
+        categories_divs = self.tree.css('div.promo-game-list')
+        if len(categories_divs) == 1:
+            last_categories = []
+            categories = CategoriesParser(categories_divs[0].html,
+                                          options=self.options.categories_parser_options).parse()
+        else:
+            last_categories = CategoriesParser(categories_divs[0].html,
+                                          options=self.options.categories_parser_options).parse()
+            categories = CategoriesParser(categories_divs[1].html,
+                                          options=self.options.categories_parser_options).parse()
+
+        secret_chat_div = self.tree.css('div.chat')[0]
+
+        return MainPage(
+            raw_source=self.tree.html,
+            header=PageHeaderParser(header_div.html, options=self.options.header_parser_options).parse(),
+            last_categories=last_categories,
+            categories=categories,
+            secret_chat=ChatParser(secret_chat_div.html, options=self.options.chat_parser_options).parse(),
+            app_data=None  # todo: make app_data parser
+        )
