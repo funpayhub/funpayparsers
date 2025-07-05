5ae94ab7c797cc0b530efff69702b3366c59e111
Qvvonk <qvvonk@gmail.com>
2025-07-05 14:59:27 +0300

    Profile page (#62)

--- funpayparsers/types/pages/__init__.py
+++ funpayparsers/types/pages/__init__.py
@@ -3,4 +3,5 @@ from .main_page import *
 from .transactions_page import *
 from .subcategory_page import *
 from .chat_page import *
+from .profile_page import *
 
---
+++ funpayparsers/types/pages/profile_page.py
@@ -0,0 +1,53 @@
+__all__ = ('ProfilePage', )
+
+
+from dataclasses import dataclass
+from funpayparsers.types.pages.base import FunPayPage
+from funpayparsers.types.offers import OfferPreview
+from funpayparsers.types.reviews import ReviewsBatch
+from funpayparsers.types.chat import Chat
+from funpayparsers.types.common import UserRating, UserBadge
+
+
+@dataclass
+class ProfilePage(FunPayPage):
+    """Represents FunPay user profile page."""
+
+    user_id: int
+    """User id."""
+
+    username: str
+    """Username."""
+
+    badge: UserBadge | None
+    """User badge."""
+
+    achievements: list[str]
+    """User achievements."""
+
+    avatar_url: str
+    """User avatar url."""
+
+    online: bool
+    """Whether the user is online or not."""
+
+    banned: bool
+    """Whether the user is banned or not."""
+
+    registration_date_text: str
+    """User registration date text."""
+
+    status_text: str
+    """User status text."""
+
+    rating: UserRating | None
+    """User rating."""
+
+    offers: dict[str, list[OfferPreview]]
+    """User offers."""
+
+    chat: Chat | None
+    """Chat with user."""
+
+    reviews: ReviewsBatch | None
+    """User reviews."""
