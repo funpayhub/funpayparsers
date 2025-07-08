8e9fb022fb1791c8cbf3e2f0c6899a7bbe7c4267
qvvonk <qvvonk@gmail.com>
2025-07-08 15:10:07 +0300

    temp

--- funpayparsers/parsers/page_parsers/order_page_parser.py
+++ funpayparsers/parsers/page_parsers/order_page_parser.py
@@ -1,6 +1,7 @@
-__all__ = ()
+__all__ = ('OrderPageParsingOptions', 'OrderPageParser')
 
 from dataclasses import dataclass
+import re
 from funpayparsers.parsers.base import FunPayHTMLObjectParser, ParsingOptions
 from funpayparsers.parsers.page_header_parser import PageHeaderParser, PageHeaderParsingOptions
 from funpayparsers.parsers.appdata_parser import AppDataParser, AppDataParsingOptions
@@ -8,6 +9,7 @@ from funpayparsers.parsers.money_value_parser import MoneyValueParser, MoneyValu
 from funpayparsers.parsers.chat_parser import ChatParser, ChatParsingOptions
 from funpayparsers.parsers.reviews_parser import ReviewsParser, ReviewsParsingOptions
 from funpayparsers.types.pages import OrderPage
+from funpayparsers.types.enums import OrderStatus
 
 
 @dataclass(frozen=True)
@@ -58,5 +60,38 @@ class OrderPageParser(FunPayHTMLObjectParser[OrderPage, OrderPageParsingOptions]
     Class for parsing order pages (`https://funpay.com/users/<user_id>/`).
     """
 
-    def parse(self):
-        ...
+    def _parse(self):
+        header_div = self.tree.css_first('header')
+        app_data = self.tree.css_first('body').attributes['data-app-data']
+
+        order_header = self.tree.css_first('h1.page-header')
+        order_id = re.search(r'#[A-Z0-9]{8}', order_header.text(deep=False).strip()).group()[1:]
+        order_status = OrderStatus.REFUNDED if order_header.css('span.text-warning') \
+            else OrderStatus.COMPLETED if order_header.css('span.text-success') \
+            else OrderStatus.PAID
+
+        goods = self.tree.css('ul.order-secrets-list')
+        if not goods:
+            delivered_goods = None
+        else:
+            delivered_goods = [i.attributes['data-copy'] for i in goods[0].css('a.btn-copy')]
+
+        review_div = self.tree.css_first('div.review-container')
+
+
+        return OrderPage(
+            raw_source=self.raw_source,
+            header=PageHeaderParser(header_div.html, options=self.options.page_header_parsing_options).parse(),
+            app_data=AppDataParser(app_data, self.options.app_data_parsing_options).parse(),
+            order_id=order_id,
+            order_status=order_status,
+            order_total=...,
+            delivered_goods=delivered_goods,
+            images=...,
+            order_category_name=...,
+            order_subcategory_name=...,
+            order_subcategory_id=...,
+            review=ReviewsParser(review_div.html, options=self.options.reviews_parsing_options).parse().reviews[0],
+            chat=...,
+            data=...
+        )
