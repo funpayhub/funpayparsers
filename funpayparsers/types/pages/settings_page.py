83a067a66f36a37424667d638b298e8e97582ba5
qvvonk <qvvonk@gmail.com>
2025-12-05 21:45:12 +0300

    Implemented `SettingsPage.from_raw_source`.

--- funpayparsers/types/pages/settings_page.py
+++ funpayparsers/types/pages/settings_page.py
@@ -4,18 +4,34 @@ from __future__ import annotations
 __all__ = ('SettingsPage',)
 
 
+from typing import TYPE_CHECKING
 from dataclasses import dataclass
 
 from funpayparsers.types.settings import Settings
 from funpayparsers.types.pages.base import FunPayPage
 
 
+if TYPE_CHECKING:
+    from funpayparsers.parsers.page_parsers.settings_page_parser import SettingsPageParsingOptions
+
+
 @dataclass
 class SettingsPage(FunPayPage):
-    """Represents user settings page (``https://funpay.com/account/settings``)."""
+    """Represents the user settings page (``https://funpay.com/account/settings``)."""
 
     settings: Settings
     """User settings."""
 
     @classmethod
-    def from_raw_source(cls, raw_source: str, options: None = None) -> SettingsPage: ...
+    def from_raw_source(
+        cls,
+        raw_source: str,
+        options: SettingsPageParsingOptions | None = None
+    ) -> SettingsPage:
+        from funpayparsers.parsers.page_parsers.settings_page_parser import (
+            SettingsPageParser,
+            SettingsPageParsingOptions,
+        )
+
+        options = options or SettingsPageParsingOptions()
+        return SettingsPageParser(raw_source=raw_source, options=options).parse()
