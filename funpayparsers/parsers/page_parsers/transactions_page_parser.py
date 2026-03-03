1fa23fbb637affd75b64fca77faee0d8841bf4a4
PythonPapochka <187691553+PythonPapochka@users.noreply.github.com>
2026-03-03 23:18:41 +0200

    add deals balance parsing to TransactionsPage

--- funpayparsers/parsers/page_parsers/transactions_page_parser.py
+++ funpayparsers/parsers/page_parsers/transactions_page_parser.py
@@ -72,7 +72,8 @@ class TransactionsPageParser(
 
     def _parse(self) -> TransactionsPage:
         money_values = []
-        for i in self.tree.css('span.balances-value'):
+        balances = self.tree.css('span.balances-value')
+        for i in balances[:3]:
             money_values.append(
                 MoneyValueParser(
                     i.text().strip(),
@@ -80,12 +81,19 @@ class TransactionsPageParser(
                     parsing_mode=MoneyValueParsingMode.FROM_STRING,
                 ).parse(),
             )
-            if len(money_values) == 3:
-                break
 
         rub_balance = [i for i in money_values if i.currency is Currency.RUB]
         usd_balance = [i for i in money_values if i.currency is Currency.USD]
         eur_balance = [i for i in money_values if i.currency is Currency.EUR]
+
+        deals_balance = None
+        if len(balances) > 3:
+            deals_text = balances[3].text().strip().split(' ', 1)[-1]
+            deals_balance = MoneyValueParser(
+                deals_text,
+                options=self.options.money_value_parsing_options,
+                parsing_mode=MoneyValueParsingMode.FROM_STRING,
+            ).parse()
 
         transactions_div = self.tree.css('div.tc-finance:not(.hidden)')
         if not transactions_div:
@@ -109,5 +117,6 @@ class TransactionsPageParser(
             rub_balance=rub_balance[0] if rub_balance else None,
             usd_balance=usd_balance[0] if usd_balance else None,
             eur_balance=eur_balance[0] if eur_balance else None,
+            deals_balance=deals_balance,
             transactions=transactions,
         )
