2347e1b302e3beb19f40bf299a22c7a8d63b9cbe
qvvonk <qvvonk@gmail.com>
2026-04-08 14:04:11 +0300

    style changes

--- funpayparsers/parsers/offer_fields_parser.py
+++ funpayparsers/parsers/offer_fields_parser.py
@@ -113,17 +113,13 @@ class OfferFieldsParser(FunPayHTMLObjectParser[OfferFields, OfferFieldsParsingOp
 
     @staticmethod
     def _parse_conditions(raw_conditions: list[dict[str, Any]]) -> list[FieldCondition]:
-        result = []
-        for cond in raw_conditions:
-            result.append(
-                FieldCondition(
-                    field_id=cond['id'],
-                    values=set(cond.get(
-                        'list', [cond['value']] if 'value' in cond else []
-                    )),
-                )
+        return [
+            FieldCondition(
+                field_id=cond['id'],
+                values=set(cond.get('list', [cond['value']] if 'value' in cond else [])),
             )
-        return result
+            for cond in raw_conditions
+        ]
 
     @staticmethod
     def _parse_options(
